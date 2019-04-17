ce8fd42fadff4c12f0f4d5cbfd77d7ccd7ec2771
LuiGee3471 <yjs3475@gmail.com>
2019-04-17 17:55:52 +0900

    dialog 추가

--- subway.js
+++ subway.js
@@ -1,5 +1,11 @@
 // 버튼 누르면 다이얼로그가 나오게
-function showDialog() {}
+function showDialog() {
+  $(".button").click(function() {
+    $(".modal").dialog({
+      autoOpen: true,
+    });
+  });
+}
 
 // 확인하면 주문 완료하기
 function complete() {}
@@ -7,6 +13,9 @@ function complete() {}
 // 주문 완료하면 주문 내역에 기록하기
 function writeRecord() {}
 
-function init() {}
+function init() {
+  const button = document.querySelector(".button");
+  button.addEventListener("click", showDialog);
+}
 
 init();
