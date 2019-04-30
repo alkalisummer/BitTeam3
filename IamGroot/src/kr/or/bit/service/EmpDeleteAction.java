72f893523422f63a7d1b15731adf6e29af49c3ac
wjfjstkrtm3 <jsy7501@gmail.com>
2019-04-30 16:28:29 +0900

    DeleteEmp

--- IamGroot/src/kr/or/bit/service/EmpDeleteAction.java
+++ IamGroot/src/kr/or/bit/service/EmpDeleteAction.java
@@ -5,13 +5,37 @@ import javax.servlet.http.HttpServletResponse;
 
 import kr.or.bit.action.Action;
 import kr.or.bit.action.ActionForward;
+import kr.or.bit.dao.EmpDao;
 
 public class EmpDeleteAction implements Action {
 
   @Override
   public ActionForward execute(HttpServletRequest request, HttpServletResponse response) {
-    // TODO Auto-generated method stub
-    return null;
+	 
+	  	ActionForward forward = new ActionForward();
+	  	
+	  	int empno = Integer.parseInt(request.getParameter("empno"));
+	  	
+	    EmpDao dao = new EmpDao();
+	    int result = dao.deleteEmp(empno);
+	    
+	    String msg = "";
+	    String url = "/list.do";
+	    
+	    if(result > 0 ) {
+	    	msg = "삭제되었습니다";
+	    } else {
+	    	msg = "삭제되지않았습니다";	
+	    }
+	    
+	    request.setAttribute("msg", msg);
+	    request.setAttribute("url", url);
+	    
+	    forward.setRedirect(false);
+	    forward.setPath("/WEB-INF/views/redirect.jsp");
+	    		
+ 	
+    return forward;
   }
 
 }
