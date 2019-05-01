438c87c8d12209045c89190e79cab3f33916d710
LuiGee3471 <yjs3475@gmail.com>
2019-05-02 00:47:19 +0900

    Register 수정

--- IamGroot/src/kr/or/bit/service/EmpUploadAction.java
+++ IamGroot/src/kr/or/bit/service/EmpUploadAction.java
@@ -5,7 +5,6 @@ import java.util.Enumeration;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 
 import com.oreilly.servlet.MultipartRequest;
 import com.oreilly.servlet.multipart.DefaultFileRenamePolicy;
@@ -19,21 +18,16 @@ public class EmpUploadAction implements Action {
 
   @Override
   public ActionForward execute(HttpServletRequest request, HttpServletResponse response) {
-    HttpSession session = request.getSession();
     String uploadpath = request.getServletContext().getRealPath("images");
     System.out.println(uploadpath);
 
     int size = 1024 * 1024 * 10;
-
-    String filename1 = "";
-    String orifilename1 = "";
-
     ActionForward forward = null;
 
     try {
       MultipartRequest multi = new MultipartRequest(request, uploadpath, size, "UTF-8", new DefaultFileRenamePolicy());
-      Enumeration filenames = multi.getFileNames();
-      String file = (String) filenames.nextElement();
+      Enumeration<String> filenames = multi.getFileNames();
+      String file = filenames.nextElement();
       String face = multi.getFilesystemName(file);
       int empno = Integer.parseInt(multi.getParameter("empno"));
       System.out.println(face + empno);
